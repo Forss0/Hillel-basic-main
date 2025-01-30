78b6d31edeff261cb6b523d21334d3d069dff9e7
Forss0 <sanivan802@gmail.com>
2025-01-30 19:07:55 +0100

    Initial commit

--- Program.cs
+++ Program.cs
@@ -1,13 +1,8 @@
-﻿Console.Write("Enter the day of the week number (1-7): ");
-if (int.TryParse(Console.ReadLine(), out int dayNumber))
-    Console.WriteLine(dayNumber 
-        switch
-    {
-        1 => "Monday",
-        2 => "Tuesday",
-        3 => "Wednesday",
-        4 => "Thursday",
-        5 => "Friday",
-        6 => "Saturday",
-        7 => "Sunday",
-    });
+﻿
+Console.Write("Enter numerator: ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter denominator: ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(b == 0 ? "Error: Division by zero" : $"Result: {a / b}");
