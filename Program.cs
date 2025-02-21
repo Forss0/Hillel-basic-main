d44bc3d968088a2ddf1defb2b6178a0fd4394911
Forss0 <sanivan802@gmail.com>
2025-02-21 17:31:56 +0100

    Initial commit

--- Program.cs
+++ Program.cs
@@ -1,26 +1,16 @@
 ﻿
+using System;
 using System.Text;
 
-Console.OutputEncoding = Encoding.Unicode;
-
-Console.Write("Введіть ім'я та прізвище: ");
-string fullName = Console.ReadLine();
-
-string[] parts = fullName.Split(' ');
-if (parts.Length < 2)
+class Program
 {
-    Console.WriteLine("Будь ласка, введіть і ім'я, і прізвище.");
-    return;
-}
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.Unicode;
 
-char firstLetterName = char.ToLower(parts[0][0]);
-char firstLetterSurname = char.ToLower(parts[1][0]);
+        Console.Write("Введіть рядок: ");
+        string input = Console.ReadLine()?.Replace(" ", "");
 
-if (firstLetterName == firstLetterSurname)
-{
-    Console.WriteLine("Прізвище починається на ту ж літеру, що і ім’я");
-}
-else
-{
-    Console.WriteLine("Прізвище не починається на ту ж літеру, що і ім’я");
+        Console.WriteLine("Результат: " + input);
+    }
 }
