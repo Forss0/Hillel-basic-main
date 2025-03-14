972455fb6240e6df10063ff8a5c642da6b1c804f
Forss0 <sanivan802@gmail.com>
2025-03-14 11:42:27 +0100

    initial commit

--- Program.cs
+++ Program.cs
@@ -1,96 +1,107 @@
 ﻿
 using System;
-using System.Text;
+using System.Collections.Generic;
 
-class TicTacToe
+abstract class Product
 {
-    static char[] board = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-    static char[] players = { 'A', 'B', 'C' };
-    static int currentPlayerIndex = 0;
+    protected double BasePrice;
 
+    public Product(double basePrice)
+    {
+        BasePrice = basePrice;
+    }
+
+    public abstract double Price { get; }
+
+    public abstract string Name { get; }
+
+    public virtual string GetInfo()
+    {
+        return $"Product: {Name}, Price: {Price}";
+    }
+}
+
+class Carrot : Product
+{
+    public Carrot(double basePrice) : base(basePrice) { }
+
+    public override double Price => BasePrice;
+
+    public override string Name => "Carrot";
+}
+
+class Potato : Product
+{
+    private double Count;
+
+    public Potato(double basePrice, double count) : base(basePrice)
+    {
+        Count = count;
+    }
+
+    public override double Price => BasePrice * Count;
+
+    public override string Name => "Potato";
+
+    public override string GetInfo()
+    {
+        return $"Product: {Name}, Price: {BasePrice}, Count: {Count}, Total price: {Price}";
+    }
+}
+
+class Cucumber : Product
+{
+    private double Count;
+
+    public Cucumber(double basePrice, double count) : base(basePrice)
+    {
+        Count = count;
+    }
+
+    public override double Price => BasePrice * Count;
+
+    public override string Name => "Cucumber";
+
+    public override string GetInfo()
+    {
+        return $"Product: {Name}, Price: {BasePrice}, Count: {Count}, Total price: {Price}";
+    }
+}
+
+class VegetableShop
+{
+    private List<Product> products = new List<Product>();
+
+    public void AddProducts(List<Product> newProducts)
+    {
+        products.AddRange(newProducts);
+    }
+
+    public void PrintProductsInfo()
+    {
+        double totalPrice = 0;
+        foreach (var product in products)
+        {
+            Console.WriteLine(product.GetInfo());
+            totalPrice += product.Price;
+        }
+        Console.WriteLine($"Total products price: {totalPrice}");
+    }
+}
+
+class Program
+{
     static void Main()
     {
-        Console.OutputEncoding = Encoding.Unicode;
-        Console.InputEncoding = Encoding.Unicode;
-
-        int move;
-        bool gameRunning = true;
-
-        while (gameRunning)
+        var products = new List<Product>
         {
-            Console.Clear();
-            PrintBoard();
-            Console.Write($"Гравець {players[currentPlayerIndex]}, оберіть номер клітинки: ");
-
-            if (int.TryParse(Console.ReadLine(), out move) && move >= 1 && move <= 9 && board[move - 1] != 'X' && board[move - 1] != 'O' && board[move - 1] != 'A' && board[move - 1] != 'B' && board[move - 1] != 'C')
-            {
-                board[move - 1] = players[currentPlayerIndex];
-                if (CheckWin())
-                {
-                    Console.Clear();
-                    PrintBoard();
-                    Console.WriteLine($"Переміг гравець {players[currentPlayerIndex]}!");
-                    gameRunning = false;
-                }
-                else if (CheckDraw())
-                {
-                    Console.Clear();
-                    PrintBoard();
-                    Console.WriteLine("Нічия!");
-                    gameRunning = false;
-                }
-                else
-                {
-                    currentPlayerIndex = (currentPlayerIndex + 1) % players.Length; //Наступний гравець
-                }
-            }
-            else
-            {
-                Console.WriteLine("Некоректний хід, спробуйте ще раз.");
-                Console.ReadLine();
-            }
-        }
-    }
-
-    static void PrintBoard()
-    {
-        Console.WriteLine($" {board[0]} | {board[1]} | {board[2]} ");
-        Console.WriteLine("-----------");
-        Console.WriteLine($" {board[3]} | {board[4]} | {board[5]} ");
-        Console.WriteLine("-----------");
-        Console.WriteLine($" {board[6]} | {board[7]} | {board[8]} ");
-    }
-
-    static bool CheckWin()
-    {
-        int[,] winCombinations =
-        {
-            {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, // Горизонталі
-            {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, // Вертикалі
-            {0, 4, 8}, {2, 4, 6}             // Діагоналі
+            new Carrot(15),
+            new Potato(20, 4),
+            new Cucumber(14, 2)
         };
 
-        for (int i = 0; i < winCombinations.GetLength(0); i++)
-        {
-            if (board[winCombinations[i, 0]] == players[currentPlayerIndex] &&
-                board[winCombinations[i, 1]] == players[currentPlayerIndex] &&
-                board[winCombinations[i, 2]] == players[currentPlayerIndex])
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    static bool CheckDraw()
-    {
-        foreach (char c in board)
-        {
-            if (char.IsDigit(c))
-            {
-                return false;
-            }
-        }
-        return true;
+        VegetableShop shop = new VegetableShop();
+        shop.AddProducts(products);
+        shop.PrintProductsInfo();
     }
 }
